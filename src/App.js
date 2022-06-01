bc82bf40ca3cb6c18665ecfc3bba1cc236a0d352
Volodymyr <volodyaruzhak@gmail.com>
2022-06-01 14:47:04 +0300

    add App.css, worked on App.js(got API amd starting stuff) implement svg Correct index.js in react 18.0.0

--- src/App.js
+++ src/App.js
@@ -1,8 +1,59 @@
 import React from "react";
+import { useEffect } from "react";
+
+import './App.css';
+import SearchIcon from './search.svg';
+
+
+const API_URL = 'https://www.omdbapi.com?apikey=3a87e849';
+
+const movie1 = {
+    "Title": "Batman: The Animated Series",
+    "Year": "1992–1995",
+    "imdbID": "tt0103359",
+    "Type": "series",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BOTM3MTRkZjQtYjBkMy00YWE1LTkxOTQtNDQyNGY0YjYzNzAzXkEyXkFqcGdeQXVyOTgwMzk1MTA@._V1_SX300.jpg"
+}
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Batman')
+    }, []);
+
     return (
-        <h1>App</h1>
+        <div className="app">
+            <h1>MovieLand</h1>
+            <div className="search">
+                <input 
+                    placeholder="Search for Movies" 
+                    value="Batman"
+                    onChange={() => {}}
+                />
+                <img src={SearchIcon} alt="search" onClick={() => {}}/>
+            </div>
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+                    <div>
+                        <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https//via.placeholder.com/400'} alt={movie1.Title} />
+                    </div>
+                    <div>
+                        <span>{movie1.Type}</span>
+                        <h3>{movie1.Title}</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
