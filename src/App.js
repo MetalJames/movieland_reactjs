dcdf444cc313521ccb8ce36cc598a9a8653455ee
Volodymyr <volodyaruzhak@gmail.com>
2022-06-01 18:18:46 +0300

    at this point complited but need to improve home page basic learning stuff

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
 
 import './App.css';
 import SearchIcon from './search.svg';
@@ -15,44 +16,52 @@ const movie1 = {
     "Poster": "https://m.media-amazon.com/images/M/MV5BOTM3MTRkZjQtYjBkMy00YWE1LTkxOTQtNDQyNGY0YjYzNzAzXkEyXkFqcGdeQXVyOTgwMzk1MTA@._V1_SX300.jpg"
 }
 
+const movie2 = {
+    "Title": "Recon 2022: The Mezzo Incident",
+    "Year": "2007",
+    "imdbID": "tt0791142",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMTY5MDE1NjAwMF5BMl5BanBnXkFtZTcwODE1MDU3MQ@@._V1_SX300.jpg"
+}
+
 const App = () => {
+    const [movies, setMovies] = useState([movie1, movie2]);
+    const [searchTerm, setSearchTerm] = useState('');
 
-    const searchMovies = async (title) => {
-        const response = await fetch(`${API_URL}&s=${title}`);
+    const searchMovies = async (year) => {
+        const response = await fetch(`${API_URL}&s=${year}`);
         const data = await response.json();
-
-        console.log(data.Search);
+        setMovies(data.Search);
+        console.log(data.Search)
     }
 
-    useEffect(() => {
-        searchMovies('Batman')
-    }, []);
+    // useEffect(() => {
+        // setMoviesMain([movie1, movie1])
+    // }, []);
 
     return (
         <div className="app">
-            <h1>MovieLand</h1>
+            <h1 className="logoHome" onClick={() => setMovies([movie1, movie2])}>MovieLand</h1>
             <div className="search">
                 <input 
                     placeholder="Search for Movies" 
-                    value="Batman"
-                    onChange={() => {}}
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                 />
-                <img src={SearchIcon} alt="search" onClick={() => {}}/>
+                <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)}/>
             </div>
-            <div className="container">
-                <div className="movie">
-                    <div>
-                        <p>{movie1.Year}</p>
-                    </div>
-                    <div>
-                        <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https//via.placeholder.com/400'} alt={movie1.Title} />
-                    </div>
-                    <div>
-                        <span>{movie1.Type}</span>
-                        <h3>{movie1.Title}</h3>
-                    </div>
+            {movies?.length > 0
+            ? (
+                <div className="container">
+                    {movies.map((movie, key)=>(
+                        <MovieCard movie={movie} key={key}/>
+                    ))}
                 </div>
-            </div>
+                ) : (
+                <div className="empty">
+                    <h2>No movies found</h2>
+                </div>
+            )}
         </div>
     );
 }
